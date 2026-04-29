01e42506a5f17c50585242ec156d186537164a8d
AmyyHsuu <shuamy11@gmail.com>
2026-04-29 11:23:03 +0800

    chore: initialize Spring Boot project

---
+++ backend/src/test/java/com/rebook/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
